6c7bce59f6e723da5cee4d791a3c44308dfc8e13
Chalcedonee <chalcedonee21@gmail.com>
2026-09-15 14:11:26 +0800

    still unfinished - ics/dev.io

--- app/admin/index.php
+++ app/admin/index.php
@@ -1,3 +1,27 @@
+<?php
+
+require_once __DIR__ . '/../../config/config.php';
+require_once __DIR__ . '/../../config/functions.php';
+
+requireRole('admin');
+
+logActivity(
+    $pdo,
+    $_SESSION['user_id'],
+    $_SESSION['user_email'],
+    'view_activity_logs',
+);
+
+    #Query get all activity logs
+    $stmt = $pdo->prepare("
+        SELECT *
+        FROM activity_logs
+        ORDER BY activity_log_timestamp DESC
+    ");
+    
+    $activities = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,5 +32,24 @@
 <body>
     <h1>Hello Admin</h1>
     <a href="../auth/signout.php">Sign out</a>
+
+    <table border="1">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>User ID</th>
+                <th>Email</th>
+                <th>Action</th>
+                <th>Status</th>
+                <th>Ip Address</th>
+                <th>User Agent</th>
+                <th>Timestamp</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php?>
+        </tbody>
+    </table>
+
 </body>
 </html>
